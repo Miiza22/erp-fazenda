7ad39d231b4ede2378ba94d53871258e1eebb586
Miiza22 <106549973+Miiza22@users.noreply.github.com>
2026-05-27 22:41:38 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "erp-fazenda-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./offline.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+
+        return cache.addAll(urlsToCache);
+
+      })
+
+  );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+    .catch(() => {
+
+      return caches.match(event.request)
+        .then((response) => {
+
+          return response || caches.match("./offline.html");
+
+        });
+
+    })
+
+  );
+
+});
